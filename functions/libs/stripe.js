d44a67d75a94b6c48fddccfbcaec5bf61eed0243
shirlyyang <shirlyyang@tencent.com>
2025-06-30 18:52:54 +0800

    add functions

--- functions/libs/stripe.js
+++ functions/libs/stripe.js
@@ -1,41 +1,41 @@
-// import Stripe from 'stripe';
-// import { FetchHttpClientResponse } from "../../node_modules/stripe/esm/net/FetchHttpClient"
-// FetchHttpClientResponse._transformHeadersToObject = (headers) => {
-//   return headers;
-// }
-// // polyfill
-// import { SubtleCryptoProvider } from "../../node_modules/stripe/esm/crypto/SubtleCryptoProvider";
-// const byteHexMapping = new Array(256);
-// for (let i = 0; i < byteHexMapping.length; i++) {
-//   byteHexMapping[i] = i.toString(16).padStart(2, '0');
-// }
-// SubtleCryptoProvider.prototype.computeHMACSignatureAsync = async function (payload, secret) {
-//   const encoder = new TextEncoder();
-//   let key;
-//   try {
-//     key = await crypto.subtle.importKey('raw', encoder.encode(secret), {
-//       name: 'HMAC',
-//       hash: 'SHA-256',
-//       // hash: { name: 'SHA-256' },
-//     }, false, ['sign']).catch(e => {
-//       console.log('computeHMACSignatureAsync error', e);
-//     });
-//   } catch (e) {
-//     console.log('computeHMACSignatureAsync error1', e);
-//   }
-//   const signatureBuffer = await this.subtleCrypto.sign('hmac', key, encoder.encode(payload));
+import Stripe from 'stripe';
+import { FetchHttpClientResponse } from "../../node_modules/stripe/esm/net/FetchHttpClient"
+FetchHttpClientResponse._transformHeadersToObject = (headers) => {
+  return headers;
+}
+// polyfill
+import { SubtleCryptoProvider } from "../../node_modules/stripe/esm/crypto/SubtleCryptoProvider";
+const byteHexMapping = new Array(256);
+for (let i = 0; i < byteHexMapping.length; i++) {
+  byteHexMapping[i] = i.toString(16).padStart(2, '0');
+}
+SubtleCryptoProvider.prototype.computeHMACSignatureAsync = async function (payload, secret) {
+  const encoder = new TextEncoder();
+  let key;
+  try {
+    key = await crypto.subtle.importKey('raw', encoder.encode(secret), {
+      name: 'HMAC',
+      hash: 'SHA-256',
+      // hash: { name: 'SHA-256' },
+    }, false, ['sign']).catch(e => {
+      console.log('computeHMACSignatureAsync error', e);
+    });
+  } catch (e) {
+    console.log('computeHMACSignatureAsync error1', e);
+  }
+  const signatureBuffer = await this.subtleCrypto.sign('hmac', key, encoder.encode(payload));
 
-//   // crypto.subtle returns the signature in base64 format. This must be
-//   // encoded in hex to match the CryptoProvider contract. We map each byte in
-//   // the buffer to its corresponding hex octet and then combine into a string.
-//   const signatureBytes = new Uint8Array(signatureBuffer);
-//   const signatureHexCodes = new Array(signatureBytes.length);
-//   for (let i = 0; i < signatureBytes.length; i++) {
-//     signatureHexCodes[i] = byteHexMapping[signatureBytes[i]];
-//   }
-//   return signatureHexCodes.join('');
-// }
-import Stripe from 'edgeone-stripe-polyfill';
+  // crypto.subtle returns the signature in base64 format. This must be
+  // encoded in hex to match the CryptoProvider contract. We map each byte in
+  // the buffer to its corresponding hex octet and then combine into a string.
+  const signatureBytes = new Uint8Array(signatureBuffer);
+  const signatureHexCodes = new Array(signatureBytes.length);
+  for (let i = 0; i < signatureBytes.length; i++) {
+    signatureHexCodes[i] = byteHexMapping[signatureBytes[i]];
+  }
+  return signatureHexCodes.join('');
+}
+// import Stripe from 'edgeone-stripe-polyfill';
 
 export let stripe;
 
